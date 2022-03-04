f42791e75864ef55867efc76bedd05f507111833
Lucas Neumann <neumannlucas@gmail.com>
2022-03-04 14:49:10 -0300

    add suport for zsh in fzhist and other minor changes

--- .config/nnn/config.sh
+++ .config/nnn/config.sh
@@ -1,10 +1,15 @@
 export NNN_OPTS="dxa"
 export NNN_OPENER=~/.config/nnn/plugins/nuke
-export NNN_PLUG='p:preview-tabbed;P:preview-tui;j:autojump;f:fzcd;F:fixname;h:fzhist;o:fzopen;r:gitroot;v:imgview;x:!chmod +x $nnn'
-export NNN_BMS='d:~/Documents;D:~/Downloads/;m:~/MEGA;d:~/.dotfiles'
 export NNN_COLORS="2136"
 export NNN_TRASH=1
-export NNN_FIFO=/tmp/nnn.fifo
+
+NNN_PLUG_PREVIEW='p:preview-tabbed;P:preview-tui'
+NNN_PLUG_FZF='f:fzcd;h:fzhist;j:autojump;o:fzopen'
+NNN_PLUG_MISC='d:dups;F:fixname;r:gitroot'
+NNN_PLUG_SHELL='l:-!git log;s:-!git status;x:!chmod +x $nnn;X:!chmod -x $nnn'
+export NNN_PLUG="$NNN_PLUG_PREVIEW;$NNN_PLUG_FZF;$NNN_PLUG_MISC;$NNN_PLUG_SHELL"
+
+export NNN_BMS='d:~/Documents;D:~/Downloads/;m:~/MEGA;.:~/.dotfiles'
 
 if [ -f /usr/share/nnn/quitcd/quitcd.bash_zsh ]; then
     source /usr/share/nnn/quitcd/quitcd.bash_zsh

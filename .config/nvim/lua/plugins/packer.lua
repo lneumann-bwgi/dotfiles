c7dd50b5c935e9e745fc19de3141f4d34a12d751
Lucas Neumann <neumannlucas@gmail.com>
2022-04-27 18:09:16 -0300

    add nvim plugins

--- .config/nvim/lua/plugins/packer.lua
+++ .config/nvim/lua/plugins/packer.lua
@@ -1,34 +1,50 @@
 -- bootstrap
 local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    packer_bootstrap = fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    })
 end
 
-return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function()
+    use("wbthomason/packer.nvim")
 
-    use 'neovim/nvim-lspconfig' -- basic lsp
-    use 'williamboman/nvim-lsp-installer' -- provides LspInstall 
+    use("neovim/nvim-lspconfig") -- basic lsp
+    use("williamboman/nvim-lsp-installer") -- provides LspInstall
+
+    -- nvim-cmp
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-cmdline")
+    use("hrsh7th/nvim-cmp")
+    use('L3MON4D3/LuaSnip')
 
     -- treesitter
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use 'nvim-treesitter/nvim-treesitter-refactor'
-    use 'nvim-treesitter/nvim-treesitter-textobjects'
-    use 'p00f/nvim-ts-rainbow'
-    use 'andymass/vim-matchup'
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    use("p00f/nvim-ts-rainbow")
 
     -- misc
-    use 'junegunn/goyo.vim'
-    use 'junegunn/limelight.vim'
-    use 'karb94/neoscroll.nvim'
-    use 'lewis6991/gitsigns.nvim'
-    use 'nvim-telescope/telescope.nvim'
-    use 'windwp/nvim-autopairs'
-    use { 'akinsho/bufferline.nvim', tag = "*", requires = 'kyazdani42/nvim-web-devicons' }
+    use("junegunn/goyo.vim")
+    use("junegunn/limelight.vim")
+
+    use("lewis6991/gitsigns.nvim")
+    use("numToStr/Comment.nvim")
+    use("nvim-telescope/telescope.nvim")
+    use("windwp/nvim-autopairs")
+    use({ "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim" } })
+
+    use("karb94/neoscroll.nvim")
+    use("lukas-reineke/indent-blankline.nvim")
+    use({ "nvim-lualine/lualine.nvim", tag = "*", requires = "kyazdani42/nvim-web-devicons" })
+    use({ "akinsho/bufferline.nvim", tag = "*", requires = "kyazdani42/nvim-web-devicons" })
 
     -- colorshemes
-    use "ellisonleao/gruvbox.nvim"
-
-
+    use("ellisonleao/gruvbox.nvim")
 end)

e76cf13cd4cf0145894d21ec65a90ccab8d90d28
Lucas Neumann <neumannlucas@gmail.com>
2022-04-28 18:04:55 -0300

    update lua plugins

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,11 @@
-require('telescope').setup {}
+local telescope = require("telescope.builtin")
+require("telescope").setup({})
 
-vim.cmd[[
-nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+vim.cmd([[
+nnoremap <leader>ff <cmd>lua require('telescope.builtin').git_files()<cr>
 nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+nnoremap <leader>ft <cmd>lua require('telescope.builtin').tags()<cr>
 nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
 nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
-]]
+nnoremap z=         <cmd>lua require('telescope.builtin').spell_suggest()<cr>
+]])

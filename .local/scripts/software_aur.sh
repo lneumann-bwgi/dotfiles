dc753bbca74d5e16bf9c760e93d9bd0a9c4e0648
neumann-mlucas <neumann.m.lucas@gmail.com>
2022-05-15 13:11:54 -0300

    add some install scripts

---
+++ .local/scripts/software_aur.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+echo
+echo "INSTALLING SOFTWARE"
+echo
+
+PKGS=(
+	"anki"
+	"brave-bin"
+	"choose-rust-git"
+	"hackernews_tui"
+	"keyd-git"
+	"megasync-bin"
+	"nnn-nerd"
+	"nyxt"
+	"picom-ibhagwan-git"
+	"so-git"
+	# "urxvt-font-size-git"
+	"yank"
+	# fonts
+	"nerd-fonts-hack"
+	"nerd-fonts-noto"
+	"otf-nerd-fonts-fira-code"
+	"ttf-iosevka-nerd"
+	"ttf-opendyslexic"
+)
+
+for PKG in "${PKGS[@]}"; do
+	echo "INSTALLING: ${PKG}"
+	yay -S "$PKG" --noconfirm --needed
+done

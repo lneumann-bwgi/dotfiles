330e6e148e70a87a420871b20dc4551673c48971
neumann-mlucas <neumann.m.lucas@gmail.com>
2022-05-31 09:03:17 -0300

    fix wrong if condition

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,7 +10,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar laptop &
 
 external_monitor=$(xrandr --query | grep 'HDMI2')
-if [[ $external_monitor = "* connected *" ]]; then
+if xrandr --query | grep 'HDMI2 connected'; then
 	polybar monitor &
 fi
 

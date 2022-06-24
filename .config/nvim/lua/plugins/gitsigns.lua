3db833fb562897f2b6e1a77563e59c4aa58e3b0a
lneumann_edeploy <lucas.neumann@e-deploy.com.br>
2022-06-24 09:16:59 -0300

    minnor changes in vim

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -20,7 +20,7 @@ require("gitsigns").setup({
 
 		["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
 		["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-		-- ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+		['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
 		-- ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
 		-- ['n <leader>hl'] = '<cmd>lua require("user").githunk.publish_loclist(true)<CR>',
 		-- ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
@@ -29,7 +29,7 @@ require("gitsigns").setup({
 		-- ['x <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
 
 		-- Text objects
-		-- ['o ih'] = ':<C-U>Gitsigns select_hunk<CR>',
-		-- ['x ih'] = ':<C-U>Gitsigns select_hunk<CR>'
+		['o ih'] = ':<C-U>Gitsigns select_hunk<CR>',
+		['x ih'] = ':<C-U>Gitsigns select_hunk<CR>'
 	},
 })
--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -6,8 +6,14 @@ require("null-ls").setup({
 		require("null-ls").builtins.formatting.gofmt,
 		require("null-ls").builtins.formatting.isort,
 		require("null-ls").builtins.formatting.stylua,
-		require("null-ls").builtins.formatting.prettier,
+		require("null-ls").builtins.formatting.prettier.with({ extra_args = { "--tab-width", "4" } }),
 		require("null-ls").builtins.formatting.shfmt,
+		require("null-ls").builtins.diagnostics.sqlfluff.with({
+			extra_args = { "--dialect", "mysql" }, -- change to your dialect
+		-- }),
+		require("null-ls").builtins.formatting.sqlfluff.with({
+			extra_args = { "--dialect", "mysql" }, -- change to your dialect
+		}),
 	},
 	on_attach = function(client)
 		if client.resolved_capabilities.document_formatting then

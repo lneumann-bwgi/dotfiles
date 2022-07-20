879969c0bce98803940173fc9ac90c761f2d247c
Lucas Neumann <neumann@devgrid.co.uk>
2022-07-20 17:06:21 -0300

    add conjure to nvim

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,3 +1,5 @@
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
 	sources = {
 		require("null-ls").builtins.formatting.black,
@@ -15,7 +17,31 @@ require("null-ls").setup({
 			extra_args = { "--dialect", "mysql" }, -- change to your dialect
 		}),
 	},
-	on_attach = function(client)
+	on_attach = function(client, bufnr)
+		--       -- from: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts#neovim-08
+		-- local lsp_formatting = function(bufnr)
+		-- 	vim.lsp.buf.format({
+		-- 		filter = function(client)
+		-- 			-- apply whatever logic you want (in this example, we'll only use null-ls)
+		-- 			return client.name == "null-ls"
+		-- 		end,
+		-- 		bufnr = bufnr,
+		-- 	})
+		-- end
+		--       -- if you want to set up formatting on save, you can use this as a callback
+		--       local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		--
+		--       -- add to your shared on_attach callback
+		-- if client.supports_method("textDocument/formatting") then
+		-- 	vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		-- 	vim.api.nvim_create_autocmd("BufWritePre", {
+		-- 		group = augroup,
+		-- 		buffer = bufnr,
+		-- 		callback = function()
+		-- 			lsp_formatting(bufnr)
+		-- 		end,
+		-- 	})
+		-- end
 		if client.resolved_capabilities.document_formatting then
 			vim.cmd([[
             augroup LspFormatting

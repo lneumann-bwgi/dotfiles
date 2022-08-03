7402b779ed7c4744fec772daaa90696d232ac1ff
Lucas Neumann <neumann@devgrid.co.uk>
2022-08-03 14:01:36 -0300

    update newsboat urls

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup({
 	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
-	ensure_installed = { "bash", "c", "go", "haskell", "html", "javascript", "json", "julia", "lua", "python", "vim" },
+	ensure_installed = { "bash", "c", "clojure", "go", "haskell", "html", "javascript", "json", "julia", "lua", "python", "vim" },
 	highlight = { enable = true, additional_vim_regex_highlightimg = false },
 	incremental_selection = { enable = false },
 	indent = { enable = true },

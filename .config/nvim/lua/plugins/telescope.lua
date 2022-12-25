fb85523b07469c5494752e5810185986c003aa36
neumann-mlucas <neumann.m.lucas@gmail.com>
2022-12-25 14:26:54 -0300

    change telescope mappings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,42 @@
-local telescope = require("telescope.builtin")
-require("telescope").setup({})
+local action_layout = require("telescope.actions.layout")
+local actions = require("telescope.actions")
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			n = {
+				["<tab>"] = action_layout.toggle_preview,
+			},
+			i = {
+				["<tab>"] = action_layout.toggle_preview,
+				["<C-s>"] = actions.cycle_previewers_next,
+				["<C-a>"] = actions.cycle_previewers_prev,
+			},
+		},
+	},
+})
 
-vim.keymap.set("n", "<leader>tb", require('telescope.builtin').buffers)
-vim.keymap.set("n", "<leader>tf", require('telescope.builtin').find_files)
-vim.keymap.set("n", "<leader>to", require('telescope.builtin').oldfiles)
-vim.keymap.set("n", "<leader>tt", require('telescope.builtin').current_buffer_tags)
-vim.keymap.set("n", "<leader>tr", require('telescope.builtin').live_grep)
-vim.keymap.set("n", "<leader>tz", require('telescope.builtin').current_buffer_fuzzy_find)
+-- find files
+vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers)
+vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
+vim.keymap.set("n", "<leader>fo", require("telescope.builtin").oldfiles)
 
-vim.keymap.set("n", "<leader>gf", require('telescope.builtin').git_files)
-vim.keymap.set("n", "<leader>gs", require('telescope.builtin').git_status)
-vim.keymap.set("n", "<leader>gc", require('telescope.builtin').git_commits)
+-- find in file
+vim.keymap.set("n", "<leader>ft", require("telescope.builtin").current_buffer_tags)
+vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
+vim.keymap.set("n", "<leader>fG", require("telescope.builtin").current_buffer_fuzzy_find)
 
-vim.keymap.set("n", "z=", require('telescope.builtin').spell_suggest)
+-- git
+vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files)
+vim.keymap.set("n", "<leader>gs", require("telescope.builtin").git_status)
+vim.keymap.set("n", "<leader>gc", require("telescope.builtin").git_commits)
+
+-- dict
+vim.keymap.set("n", "z=", require("telescope.builtin").spell_suggest)
+
+-- lsp
+vim.keymap.set("n", "<leader>fr", require("telescope.builtin").lsp_references)
+vim.keymap.set("n", "<leader>fd", require("telescope.builtin").lsp_definitions)
+vim.keymap.set("n", "<leader>ic", require("telescope.builtin").lsp_incoming_calls)
+vim.keymap.set("n", "<leader>oc", require("telescope.builtin").lsp_outgoing_calls)
+
+vim.keymap.set("n", "<leader>d", ":Telescope diagnostics bufnr=0<cr>")

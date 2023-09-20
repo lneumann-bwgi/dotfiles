09421bf14edd4d6fc0e2162b1995bf0c98b929ac
neumann-mlucas <neumann.m.lucas@gmail.com>
2023-09-19 21:30:09 -0300

    swap lsp-installer for mason

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -1,61 +1,46 @@
--- FROM: https://github.com/neovim/nvim-lspconfig
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"bashls",
+		"dockerls",
+		"gopls",
+		"html",
+		"jsonls",
+		"julials",
+		"pyright",
+		"sqlls",
+		"lua_ls",
+		"tsserver",
+		"vimls",
+	},
+})
 
-local api = vim.api
-local opts = { noremap = true, silent = true }
-api.nvim_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-api.nvim_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-api.nvim_set_keymap("n", "<space>D", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+require("mason-lspconfig").setup_handlers({
+	function(server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup({})
+	end,
+})
 
--- Use an on_attach function to only map the following keys
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set("n", "<space>D", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+
+-- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-	-- Enable completion triggered by <c-x><c-o>
-	api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-	api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-	api.nvim_buf_set_keymap(bufnr, "n", "gh", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-	api.nvim_buf_set_keymap(bufnr, "n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-	api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-	api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-	api.nvim_buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-	api.nvim_buf_set_keymap(bufnr, "n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
-	if client.server_capabilities.documentHighlightProvider then
-		api.nvim_command("autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()")
-		api.nvim_command("autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()")
-		api.nvim_command("autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()")
-	end
-end
-
--- from : https://github.com/williamboman/nvim-lsp-installer/issues/537
-local lsp_installer = require("nvim-lsp-installer")
-lsp_installer.on_server_ready(function(server)
-	local server_opts = {
-		on_attach = on_attach,
-	}
-	server:setup(server_opts)
-end)
-
--- from: https://github.com/williamboman/nvim-lsp-installer/wiki/Advanced-Configuration#automatically-install-lsp-servers
-local servers = {
-	"bashls",
-	"clojure_lsp",
-	"dockerls",
-	"gopls",
-	"html",
-	"jsonls",
-	"julials",
-	"pyright",
-	-- "hls",
-	"slangd",
-	"sqlls",
-	"lua_ls",
-	"tsserver",
-	"vimls",
-}
-for _, name in pairs(servers) do
-	local server_is_found, server = lsp_installer.get_server(name)
-	if server_is_found and not server:is_installed() then
-		print("Installing " .. name)
-		server:install()
-	end
-end
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+	end,
+})

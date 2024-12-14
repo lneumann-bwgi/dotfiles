858c0b0aed134a766017401c4b7e28e5abaf2f05
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-12-14 18:36:54 -0300

    add conform.nvim to deprecate null-ls

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,50 @@
+local M = {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				c = { "clang_format" },
+				go = { "goimports", "gofmt" },
+				javascript = { "prettier" },
+				json = { "prettier" },
+				lua = { "stylua" },
+				python = { "ruff_format" },
+				sh = { "shfmt" },
+				sql = { "sqlfluff" },
+				["*"] = { "codespell" },
+				["_"] = { "trim_whitespace" },
+			},
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+				sqlfluff = {
+					prepend_args = { "fix", "--force", "--dialect=ansi", "-" },
+				},
+			},
+			-- If this is set, Conform will run the formatter on save.
+			-- It will pass the table to conform.format().
+			-- This can also be a function that returns the table.
+			format_on_save = {
+				lsp_format = "fallback",
+				timeout_ms = 500,
+			},
+			-- Set the log level. Use `:ConformInfo` to see the location of the log file.
+			log_level = vim.log.levels.ERROR,
+			-- Conform will notify you when a formatter errors
+			notify_on_error = true,
+			-- Conform will notify you when no formatters are available for the buffer
+			notify_no_formatters = true,
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
+}
+return M
--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,53 +1,18 @@
-local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
-end
-
--- if you want to set up formatting on save, you can use this as a callback
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
--- add to your shared on_attach callback
-local on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end,
-        })
-    end
-end
-
 local M = {
-    "nvimtools/none-ls.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.code_actions.gitsigns,
-                null_ls.builtins.completion.spell,
-                null_ls.builtins.diagnostics.hadolint,
-                null_ls.builtins.diagnostics.mypy,
-                null_ls.builtins.diagnostics.sqlfluff,
-                null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.clang_format,
-                null_ls.builtins.formatting.gofmt,
-                null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.shfmt,
-                null_ls.builtins.formatting.sqlfluff,
-                null_ls.builtins.formatting.stylua,
-            },
-            on_attach = on_attach,
-        })
-    end,
+	"nvimtools/none-ls.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.code_actions.gitsigns,
+				null_ls.builtins.diagnostics.codespell,
+				null_ls.builtins.diagnostics.hadolint,
+				null_ls.builtins.diagnostics.ruff,
+				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.diagnostics.sqlfluff,
+			},
+		})
+	end,
 }
-
 return M

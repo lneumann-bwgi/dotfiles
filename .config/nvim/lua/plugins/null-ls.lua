d3b2212d5331ea55d49b30efa209c24f261fb3fd
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-12-15 18:27:55 -0300

    remove null-ls

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,18 +1,18 @@
-local M = {
-	"nvimtools/none-ls.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.code_actions.gitsigns,
-				null_ls.builtins.diagnostics.codespell,
-				null_ls.builtins.diagnostics.hadolint,
-				null_ls.builtins.diagnostics.ruff,
-				null_ls.builtins.diagnostics.mypy,
-				null_ls.builtins.diagnostics.sqlfluff,
-			},
-		})
-	end,
-}
+local M = {}
+-- local M = {
+--     "nvimtools/none-ls.nvim",
+--     dependencies = { "nvim-lua/plenary.nvim" },
+--     config = function()
+--         local null_ls = require("null-ls")
+--         null_ls.setup({
+--             sources = {
+--                 null_ls.builtins.code_actions.gitsigns,
+--                 null_ls.builtins.diagnostics.codespell,
+--                 null_ls.builtins.diagnostics.hadolint,
+--                 null_ls.builtins.diagnostics.mypy,
+--                 null_ls.builtins.diagnostics.sqlfluff,
+--             },
+--         })
+--     end,
+-- }
 return M

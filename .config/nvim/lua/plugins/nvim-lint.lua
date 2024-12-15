86d7d500ac8e53d8812fbe3275279b64e32408e6
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-12-15 18:59:48 -0300

    add nvim-lint (testing)

---
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,29 @@
+local M = {
+    "mfussenegger/nvim-lint",
+    event = {
+        "BufReadPre",
+        "BufNewFile",
+    },
+    config = function()
+        local lint = require("lint")
+
+        lint.linters_by_ft = {
+            markdown = { "vale" },
+            python = { "mypy", "ruff" },
+        }
+
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+            group = lint_augroup,
+            callback = function()
+                lint.try_lint()
+            end,
+        })
+
+        vim.keymap.set("n", "<leader>ll", function()
+            lint.try_lint()
+        end, { desc = "Trigger linting for current file" })
+    end,
+}
+return M

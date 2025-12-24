9aeecc1d5f073d0504e0cdd30d3ba0e05848ae8c
lucas.neumann <lucas.neumann@bwgi.com.br>
2025-12-24 18:34:09 -0300

    vim: change spell lint

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -22,19 +22,20 @@ local M = {
       "--dialect",
       "postgres",
     }
+    local adn_and_or = "ajskdnasd"
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
-        lint.try_lint("codespell")
+        lint.try_lint("typos")
       end,
     })
 
     vim.keymap.set("n", "<leader>ll", function()
       lint.try_lint()
-      lint.try_lint("codespell")
+      lint.try_lint("typos")
     end, { desc = "Trigger linting for current file" })
   end,
 }

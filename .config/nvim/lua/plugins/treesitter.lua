f6b74626580c20e480c91a1d505dfdb1794d3323
lucas.neumann <lucas.neumann@bwgi.com.br>
2026-06-17 10:52:59 -0300

    vim: remove stale branch from lazy setup

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,80 +1,60 @@
+local parsers = {
+  "bash", "c", "clojure", "go", "haskell", "html", "javascript", "json",
+  "julia", "lua", "python", "vim", "markdown", "markdown_inline", "regex",
+}
+
 local M = {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  event = "VeryLazy",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   dependencies = {
-    { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
+    { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
     "windwp/nvim-ts-autotag",
   },
   config = function()
-    require("nvim-treesitter.configs").setup({
-      -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-      ensure_installed = {
-        "bash",
-        "c",
-        "clojure",
-        "go",
-        "haskell",
-        "html",
-        "javascript",
-        "json",
-        "julia",
-        "lua",
-        "python",
-        "vim",
-        "markdown",
-        "markdown_inline",
-        "regex",
-      },
-      highlight = { enable = true, additional_vim_regex_highlighting = false },
-      incremental_selection = { enable = false },
-      indent = { enable = true },
-      -- See: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-      textobjects = {
-        select = {
-          enable = true,
-          -- Automatically jump forward to textobj, similar to targets.vim
-          lookahead = true,
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["il"] = "@loop.inner",
-          },
-          selection_modes = {
-            ["@function.outer"] = "V",
-            ["@function.inner"] = "V",
-            ["@class.outer"] = "V",
-            ["@class.inner"] = "V",
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]C"] = "@class.outer",
-          },
-          goto_next_end = {
-            ["]F"] = "@function.outer",
-            -- ["]C"] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[C"] = "@class.outer",
-          },
-          goto_previous_end = {
-            ["[F"] = "@function.outer",
-            -- ["[C"] = "@class.outer",
-          },
+    require("nvim-treesitter").setup()
+    require("nvim-treesitter").install(parsers)
+
+    require("nvim-treesitter-textobjects").setup({
+      select = {
+        lookahead = true,
+        selection_modes = {
+          ["@function.outer"] = "V",
+          ["@function.inner"] = "V",
+          ["@class.outer"] = "V",
+          ["@class.inner"] = "V",
         },
       },
+      move = { set_jumps = true },
     })
 
     require("nvim-ts-autotag").setup()
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = parsers,
+      callback = function(ev)
+        pcall(vim.treesitter.start, ev.buf)
+        vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+
+    local select = require("nvim-treesitter-textobjects.select")
+    local move = require("nvim-treesitter-textobjects.move")
+    local map = function(modes, lhs, fn) vim.keymap.set(modes, lhs, fn) end
+
+    map({ "x", "o" }, "af", function() select.select_textobject("@function.outer", "textobjects") end)
+    map({ "x", "o" }, "if", function() select.select_textobject("@function.inner", "textobjects") end)
+    map({ "x", "o" }, "ac", function() select.select_textobject("@class.outer", "textobjects") end)
+    map({ "x", "o" }, "ic", function() select.select_textobject("@class.inner", "textobjects") end)
+    map({ "x", "o" }, "il", function() select.select_textobject("@loop.inner", "textobjects") end)
+
+    map({ "n", "x", "o" }, "]f", function() move.goto_next_start("@function.outer", "textobjects") end)
+    map({ "n", "x", "o" }, "]C", function() move.goto_next_start("@class.outer", "textobjects") end)
+    map({ "n", "x", "o" }, "]F", function() move.goto_next_end("@function.outer", "textobjects") end)
+    map({ "n", "x", "o" }, "[f", function() move.goto_previous_start("@function.outer", "textobjects") end)
+    map({ "n", "x", "o" }, "[C", function() move.goto_previous_start("@class.outer", "textobjects") end)
+    map({ "n", "x", "o" }, "[F", function() move.goto_previous_end("@function.outer", "textobjects") end)
   end,
 }
 return M
